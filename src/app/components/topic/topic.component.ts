4d2215fc0c0871f2f6447423759df9aa500bea79
BogdanBlanaru <bogdansebastian200@yahoo.com>
2023-11-15 15:57:32 +0200

    create project structure

---
+++ src/app/components/topic/topic.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topic',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './topic.component.html',
+  styleUrl: './topic.component.scss'
+})
+export class TopicComponent {
+
+}
