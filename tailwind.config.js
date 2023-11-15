588f0d66d8a320645ded43b8b9cdcd9036f185e3
Blănaru Bogdan Sebastian <100227050+BogdanBlanaru@users.noreply.github.com>
2023-11-15 18:54:33 +0200

    configure tailwind settings in app (#2)

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // tell tailwind to search for classes in these type of files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
